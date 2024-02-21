d668d500d7a55230f154f39ec0f988d7437c61db
patrickreinan <patrickreinan@gmail.com>
2024-02-21 16:27:56 -0300

    change args

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ LLAMA_CPP_MODELS_DIR=$LLAMA_CPP_DIR/models
 LLAMA_MODELS_DIR=/Users/patrickreinan/git/llama-models
 
 
-noclone=0
-nobuild=0
-nodownload=0
+clone=0
+build=0
+download=0
 
 
 OPTSTRING=":cbd"
@@ -19,13 +19,13 @@ while getopts $OPTSTRING flag
 do
     case ${flag} in
         c) 
-            noclone=1
+            clone=1
             ;;
         b) 
-            nobuild=1
+            build=1
             ;;
         d) 
-            nodownload=1
+            download=1
             ;;
     esac
 done
@@ -33,37 +33,39 @@ done
 
 
 
-if [  $noclone -eq 0 ] 
+if [  $clone -eq 1 ] 
 then
     rm -rf $LLAMA_CPP_DIR
     cd $GIT_DIR
     git clone https://github.com/ggerganov/llama.cpp.git
+    download=1
 fi
 
 
 cd $LLAMA_CPP_DIR
 
 
-if [  $nobuild -eq 0 ] 
+if [  $build -eq 1  ] || [ $clone -eq 1 ]
 then
-    git checkout 1aa18ef # workaround https://gist.github.com/adrienbrault/b76631c56c736def9bc1bc2167b5d129#:~:text=git-,checkout,-1aa18ef
+    #git checkout 1aa18ef # workaround https://gist.github.com/adrienbrault/b76631c56c736def9bc1bc2167b5d129#:~:text=git-,checkout,-1aa18ef
     make clean
     LLAMA_METAL=1 make
 fi
 
-# Download model
+# download model
 export MODEL=llama-2-13b-chat.ggmlv3.q4_0.bin
 
-if [  $nodownload -eq 0 ] 
+if [  $download -eq 1 ] 
 then
     #wget "https://huggingface.co/TheBloke/Llama-2-13B-chat-GGML/resolve/main/${MODEL}"
     cp $LLAMA_MODELS_DIR/$MODEL $LLAMA_CPP_MODELS_DIR/
 
 fi
 
-echo "Prompt: " \
-    && read PROMPT \
-    &&  ./main \
+#echo "Prompt: " \
+    # && read PROMPT \
+    #&&  
+    ./main \
         --threads 8 \
         --n-gpu-layers 1 \
         --model models/${MODEL} \
@@ -72,4 +74,8 @@ echo "Prompt: " \
         --temp 0.7 \
         --repeat_penalty 1.1 \
         --n-predict -1 \
-        --prompt "[INST] $PROMPT [/INST]"
+        -i \
+        -r "User:" \
+        --in-suffix "Llama" \
+        --in-prefix " "
+        #--prompt "[INST] $PROMPT [/INST]"
